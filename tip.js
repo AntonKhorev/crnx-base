e6e8758317062d0ee6d6095aeccdb9793f54ed1e
Anton Khorev <tony29@yandex.ru>
2016-05-10 21:52:23 +0300

    delay for tip popup on mouseover, but not on focus

--- tip.js
+++ tip.js
@@ -4,29 +4,29 @@ module.exports=(tipClass,tipContent)=>{
 	const $tip=$(`<span class='tip-${tipClass}' tabindex='0'><span class='tip-content'>${tipContent}</span></span>`)
 	let popupTimeoutId=null
 	let popupLevel=0
-	const handlePopup=()=>{
-		if (popupLevel) {
-			clearTimeout(popupTimeoutId)
-			popupTimeoutId=null
-			$tip.addClass('hold')
-		} else {
-			popupTimeoutId=setTimeout(()=>{
-				$tip.removeClass('hold')
-			},300)
+	const handlePopup=(dLevel,delay)=>{
+		if (delay===undefined) {
+			delay=200
 		}
+		if (dLevel==+1 && popupLevel==0) {
+			clearTimeout(popupTimeoutId)
+			popupTimeoutId=setTimeout(()=>{ // sort of hoverintent, simple delay will do b/c tip icon is small
+				$tip.addClass('open')
+			},delay)
+		} else if (dLevel==-1 && popupLevel==1) {
+			clearTimeout(popupTimeoutId)
+			popupTimeoutId=setTimeout(()=>{
+				$tip.removeClass('open')
+			},delay)
+		}
+		popupLevel+=dLevel
 	}
-	$tip.on('mouseenter focus',function(){
-		popupLevel+=1
-		handlePopup()
-	}).on('mouseleave blur',function(){
-		popupLevel-=1
-		handlePopup()
-	}).on('focus','*',function(){ // keyboard focus transferred from tip itself to something inside it
-		popupLevel+=1
-		handlePopup()
-	}).on('blur','*',function(){
-		popupLevel-=1
-		handlePopup()
+	$tip.mouseenter(function(){
+		handlePopup(+1)
+	}).focusin(function(){
+		handlePopup(+1,0) // need immediate popup on focusin
+	}).on('mouseleave focusout',function(){
+		handlePopup(-1)
 	})
 	return $tip
 }
