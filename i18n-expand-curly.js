037c7fe02f4037581f16635ad64da1d47b74754c
Anton Khorev <tony29@yandex.ru>
2016-08-24 04:19:46 +0300

    use destructuring in i18n-expand-curly.js

--- i18n-expand-curly.js
+++ i18n-expand-curly.js
@@ -4,16 +4,14 @@ module.exports=function(inStrings){
 	const strings={}
 	const expandRegexp=/^([^{]*)\{([^}]*)\}(.*)$/
 	function expandIdAndString(id,string) {
-		let match
-		if (match=expandRegexp.exec(id)) {
-			const idStart=match[1]
-			const idMids=match[2].split(',')
-			const idEnd=match[3]
-			if (match=expandRegexp.exec(string)) {
-				const stringStart=match[1]
-				const stringMidsStr=match[2]
+		const idMatch=expandRegexp.exec(id)
+		if (idMatch) {
+			const [,idStart,idMidsStr,idEnd]=idMatch
+			const idMids=idMidsStr.split(',')
+			const stringMatch=expandRegexp.exec(string)
+			if (stringMatch) {
+				const [,stringStart,stringMidsStr,stringEnd]=stringMatch
 				const stringMids=stringMidsStr.split(',')
-				const stringEnd=match[3]
 				idMids.forEach((idMid,i)=>{
 					const stringMid=stringMids[i]
 					if (stringMidsStr!='') {
