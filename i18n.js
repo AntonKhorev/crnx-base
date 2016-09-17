b3e93c3f7a93d1e32346aab4dcf96ac95a4cd60e
Anton Khorev <tony29@yandex.ru>
2016-09-17 03:54:32 +0300

    additional number/unit fns in i18n

--- i18n.js
+++ i18n.js
@@ -21,10 +21,8 @@ module.exports=langStrings=>lang=>{
 	i18n.lang=lang
 	i18n.has=id=>strings[id]!==undefined
 	if (lang=='en') {
-		i18n.number=function(n){
-			return String(n).replace('-','−')
-		}
-		i18n.pluralSuffix=function(n){
+		i18n.number=(n)=>String(n).replace('-','−')
+		i18n.pluralSuffix=(n)=>{
 			const ns=String(n)
 			if (ns/*:string*/.includes('.')) {
 				return '2'
@@ -38,10 +36,8 @@ module.exports=langStrings=>lang=>{
 			}
 		}
 	} else if (lang=='ru') {
-		i18n.number=function(n){
-			return String(n).replace('-','−').replace('.',',')
-		}
-		i18n.pluralSuffix=function(n){
+		i18n.number=(n)=>String(n).replace('-','−').replace('.',',')
+		i18n.pluralSuffix=(n)=>{
 			const ns=String(n)
 			if (ns/*:string*/.includes('.')) {
 				return '2'
@@ -57,30 +53,49 @@ module.exports=langStrings=>lang=>{
 			}
 		}
 	}
-	i18n.plural=function(n,id){
-		return this(id+'.'+this.pluralSuffix(n))
-	}
-	i18n.numberWithUnits=function(n,unit,abbrFn){
+	i18n.plural=(n,id)=>i18n(id+'.'+i18n.pluralSuffix(n))
+	const haveToAttachUnit=unit=>(unit.length==1 && /^\W/.test(unit))
+	const expandedUnit=(n,unit,abbrFn)=>{
 		if (abbrFn===undefined) { // codegen needs its own fn
 			abbrFn=(abbr,expansion)=>"<abbr title='"+escape(expansion)+"'>"+abbr+"</abbr>" // expansion is html for stuff like s^(-1)
 		}
-		let s=this.number(n)
-		if (unit!==undefined) {
-			if (unit.length==1 && /^\W/.test(unit)) {
-				s+=unit
-			} else {
-				s+=' ' // nbsp
-				let abbr
-				if (unit=='1/second') {
-					abbr=i18n('units.second.a')+"<sup>−1</sup>"
-				} else {
-					abbr=i18n('units.'+unit+'.a')
-				}
-				const expansion=this.plural(n,'units.'+unit)
-				s+=abbrFn(abbr,expansion)
-			}
+		let abbr
+		if (unit=='1/second') {
+			abbr=i18n('units.second.a')+"<sup>−1</sup>"
+		} else {
+			abbr=i18n('units.'+unit+'.a')
 		}
-		return s
+		const expansion=i18n.plural(n,'units.'+unit)
+		return abbrFn(abbr,expansion)
+	}
+	i18n.numberWithUnits=(n,unit,abbrFn)=>{
+		let s=i18n.number(n)
+		if (unit===undefined) {
+			return s
+		} else if (haveToAttachUnit(unit)) {
+			return s+unit
+		} else {
+			return s+' '+expandedUnit(n,unit,abbrFn)
+		}
+	}
+	// example:
+	// 1 .. 4 seconds = (1)(4)(seconds)
+	// 1% .. 4% = (1%)(4%)()
+	// made with 3 calls: (numberWithoutUnits)(numberWithoutUnits)(numberUnits)
+	i18n.numberWithoutUnits=function(n,unit){
+		let s=i18n.number(n)
+		if (unit!==undefined && haveToAttachUnit(unit)) {
+			return s+unit
+		} else {
+			return s
+		}
+	}
+	i18n.numberUnits=function(n,unit,abbrFn){
+		if (unit!==undefined && !haveToAttachUnit(unit)) {
+			return expandedUnit(n,unit,abbrFn)
+		} else {
+			return ''
+		}
 	}
 	return i18n
 }
