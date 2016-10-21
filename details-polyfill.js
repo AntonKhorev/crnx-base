1724b2bb46e94da4667572831504d4b1b309bce2
Anton Khorev <tony29@yandex.ru>
2016-10-21 16:49:19 +0300

    <details> fix for firefox

--- details-polyfill.js
+++ details-polyfill.js
@@ -3,8 +3,11 @@
 const detailsSupported=('open' in document.createElement('details')) // http://thenewcode.com/680/Feature-Detection-and-Styling-For-The-HTML5-details-Element
 
 module.exports=function(){ // to be called in .each()
-	if (detailsSupported) return
 	const $details=$(this)
+	$details.on('click','summary :input, summary a[href]',function(ev){ // firefox lets those events through
+		ev.preventDefault()
+	})
+	if (detailsSupported) return
 	const toggleOpen=()=>{
 		if (!$details.attr('open')) {
 			$details.attr('open','')
